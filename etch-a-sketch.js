ea6298549f5b9383966eaecf9e1b970a0e64f880
leocodesthings <106573277+leocodesthings@users.noreply.github.com>
2023-10-24 12:51:02 +1000

    implement board coloring on mouseover

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,6 @@
 let board = document.querySelector('#board');
-
+let pixelMark = document.querySelector('.pixel');
+//let pixelMark = board.querySelectorAll('.pixel');
 //
 // document.addEventListener("DOMContentLoaded", () => {
 //
@@ -12,4 +13,29 @@ function etchBoard() {
     }
 }
 
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+}
+
+function markBoard() {
+    document.addEventListener("mouseover", (event) => {
+        document.body.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    })
+}
+
+// pixelMark.addEventListener("mouseover", () => {
+//     pixelMark.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+// })
+
+
+// pixelMark.forEach() => {
+//     //where to add event listener? what type of listener?
+//     pixelMark.addEventListener("mouseover", (event) => {
+//        //what event do you want to happen?
+//         pixelMark.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+//     });
+//
+// }
+
+markBoard();
 etchBoard();
